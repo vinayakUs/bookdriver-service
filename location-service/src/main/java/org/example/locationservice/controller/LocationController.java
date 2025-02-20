fa7fd24da8654c0f3c554a2a31d78a690613a456
vinayakUs <vinayak200054321@gmail.com>
2025-02-20 19:21:31 +0530

    wip

---
+++ location-service/src/main/java/org/example/locationservice/controller/LocationController.java
@@ -0,0 +1,26 @@
+package org.example.locationservice.controller;
+
+import com.google.maps.errors.ApiException;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.example.locationservice.service.LocationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/location")
+@RequiredArgsConstructor
+public class LocationController {
+
+    private final LocationService locationService;
+
+    @GetMapping("/autocomplete")
+    public ResponseEntity<?> autocomplete(@RequestParam("query") String query,@RequestParam("sessionToken") String sessionToken) throws IOException, InterruptedException, ApiException {
+        return ResponseEntity.ok().body(locationService.getPlaceAutocomplete(query,sessionToken));
+    }
+}
