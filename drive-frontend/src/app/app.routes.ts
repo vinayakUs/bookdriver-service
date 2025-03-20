843cc7b9ab947d643855fdb4e9311bb145e32371
vinayakaUs <vinayak200054321@gmail.com>
2025-03-20 22:01:56 +0530

    new changes

--- drive-frontend/src/app/app.routes.ts
+++ drive-frontend/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import {LoginComponent} from './component/login/login.component';
 import {authGuard} from './auth.guard';
 import {AppComponent} from './app.component';
 import {RegisterComponent} from './component/register/register.component';
+import { ResetComponent } from './component/reset/reset.component';
+import { MailsendComponent } from './component/mailsend/mailsend.component';
+import { ResetsuccessComponent } from './component/resetsuccess/resetsuccess.component';
 
 // export const routes: Routes = [
 //   { path: 'login', component: LoginComponent },
@@ -26,6 +29,10 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [authGuard] }, // Protected
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'reset', component: ResetComponent },
+  { path: 'send', component: MailsendComponent },
+  { path: 'resetSuccess', component: ResetsuccessComponent },
+
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' }
 ];
