4b665055ee79837305e9df5066a4eb929347fe54
vinayakUs <vinayak200054321@gmail.com>
2025-03-20 14:24:35 +0530

    new login register

--- drive-frontend/src/app/component/login/login.component.ts
+++ drive-frontend/src/app/component/login/login.component.ts
@@ -1,33 +1,67 @@
 import { Component } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import {AuthService} from '../../service/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: [],
+  // styleUrls: [],
+  styleUrl:'login.component.css',
   standalone: true,
   imports: [FormsModule, CommonModule, ReactiveFormsModule]
 })
 export class LoginComponent {
+  isLoading: boolean = false;
   loginForm = new FormGroup({
-    email: new FormControl('sa@gmail.com'),
-    password: new FormControl('sa')
+    email: new FormControl(),
+    password: new FormControl()
   });
 
 
-  user = {
-    email: '',
-    password: ''
-  };
+  constructor(private router: Router,private authService: AuthService) {
+    if(this.authService.isLoggedIn()){
+      this.router.navigate(['/home']).then(r => console.log("successfully redirected to home")); // Redirect to home if already logged in
+    }
+  }
 
-  constructor(private router: Router) {}
 
   onSubmit() {
-    console.log('Login form submitted:', this.user);
-    // TODO: Implement actual login logic
+    if (this.loginForm.valid && !this.isLoading) {
+      this.isLoading = true;
+      const credentials = {
+        email: this.loginForm.value.email!,
+        password: this.loginForm.value.password!,
+      };
+
+      this.authService.login(credentials).subscribe({
+        next: () => {
+          console.log('Login successful');
+          this.isLoading = false;
+          this.router.navigate(['/home']);
+        },
+        error: err => {
+          this.isLoading = false;
+          console.error('Login failed', err);
+        }
+      });
+    }
   }
+
+
+
+
+
+
+
+
+
+
+  // onSubmit() {
+  //   console.log('Login form submitted:', this.user);
+  //   // TODO: Implement actual login logic
+  // }
 
   goToRegister() {
     this.router.navigate(['/register']);
--- drive-frontend/src/app/component/register/register.component.ts
+++ drive-frontend/src/app/component/register/register.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NgIf} from '@angular/common';
+import {Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from '../../service/auth.service';
 
 @Component({
   selector: 'app-register',
-    imports: [
-        FormsModule,
-        ReactiveFormsModule
-    ],
+  standalone: true,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  isRegistering: boolean = true;
+
+  constructor(private http: HttpClient, private router: Router,private authService: AuthService) {
+    console.log("logged in",this.authService.isLoggedIn());
+    if(this.authService.isLoggedIn()){
+      console.log("logged in",this.authService.isLoggedIn());
+      this.router.navigate(['/home']).then(r => console.log("successfully redirected to home")); // Redirect to home if already logged in
+
+    }
+  }
 
 }
