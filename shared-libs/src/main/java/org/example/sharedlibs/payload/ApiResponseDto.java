3f01d43e9112b9e7e09cb1b241e19d9f33e49e57
vinayakUs <vinayak200054321@gmail.com>
2025-04-15 03:43:23 +0530

    Added api_response dto shared

---
+++ shared-libs/src/main/java/org/example/sharedlibs/payload/ApiResponseDto.java
@@ -0,0 +1,55 @@
+package org.example.sharedlibs.payload;
+
+import java.time.Instant;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+/**
+ * A generic API response DTO.
+ * This class can be extended to create specific response DTOs for different API
+ * endpoints.
+ */
+public class ApiResponseDto<T> {
+    private T data;
+    private boolean success;
+    private String path;
+    private String cause;
+    private String timestamp;
+
+    public ApiResponseDto(boolean success, T data) {
+        this.timestamp = Instant.now().toString();
+        this.success = success;
+        this.data = data;
+        this.path = null;
+        this.cause = null;
+    }
+
+    public ApiResponseDto(boolean success, T data, String cause, String path) {
+        this.timestamp = Instant.now().toString();
+        this.success = success;
+        this.data = data;
+        this.path = path;
+        this.cause = cause;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
